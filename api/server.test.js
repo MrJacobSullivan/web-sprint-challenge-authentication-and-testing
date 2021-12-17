cd2e2456fb7988da2fdefaf9e3aa1a8224baef32
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-17 14:39:50 -0600

    added users-model.test.js

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,42 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+const bcrypt = require('bcryptjs')
+const { BCRYPT_ROUNDS } = require('../config')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
 })
+
+beforeEach(() => {
+  const users = [
+    { username: 'jacob', password: bcrypt.hashSync('1234', BCRYPT_ROUNDS) },
+    { username: 'emma', password: bcrypt.hashSync('1234', BCRYPT_ROUNDS) },
+  ]
+
+  users.forEach(async (user) => {
+    db('users').insert(user)
+  })
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
+it('is the correct env', () => {
+  expect(process.env.NODE_ENV).toBe('testing')
+})
+
+describe('users router', () => {
+  describe('[POST] /api/auth/register', () => {
+    let res
+
+    // beforeEach(async () => {})
+  })
+
+  describe('[POST] /api/auth/login', () => {})
+})
+
+describe('jokes router', () => {})
--- api/users/users-model.test.js
+++ api/users/users-model.test.js
@@ -1,1 +1,63 @@
-// TODO:
+const db = require('../../data/dbConfig')
+const Users = require('./users-model')
+
+const bcrypt = require('bcryptjs')
+const { BCRYPT_ROUNDS } = require('../../config')
+
+beforeEach(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+
+  // seed the db with some test users
+  const users = [
+    { username: 'jacob', password: bcrypt.hashSync('1234', BCRYPT_ROUNDS) },
+    { username: 'emma', password: bcrypt.hashSync('hunter2', BCRYPT_ROUNDS) },
+  ]
+
+  users.forEach(async (user) => {
+    await db('users').insert(user)
+  })
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('users model', () => {
+  describe('get', () => {
+    it('resolves all users in users table', async () => {
+      const result = await Users.get()
+      expect(result).toHaveLength(2)
+    })
+  })
+
+  describe('getBy', () => {
+    const expected = { id: 1, username: 'jacob' }
+
+    it('resolves a user with given id', async () => {
+      const result = await Users.getBy({ id: 1 })
+      expect(result).toMatchObject(expected)
+    })
+
+    it('resolves a user with a given username', async () => {
+      const result = await Users.getBy({ username: 'jacob' })
+      expect(result).toMatchObject(expected)
+    })
+  })
+
+  describe('insert', () => {
+    const input = { username: 'larry', password: bcrypt.hashSync('password1', BCRYPT_ROUNDS) }
+    const expected = { id: 3, username: 'larry' }
+
+    it('creates a new user in db', async () => {
+      await Users.insert(input)
+      const [larry] = await db('users').where({ id: 3 })
+      expect(larry).toMatchObject(expected)
+    })
+
+    it('resolves the new user with id, username', async () => {
+      const result = await Users.insert(input)
+      expect(result).toMatchObject(expected)
+    })
+  })
+})
